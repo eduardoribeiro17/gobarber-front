8d8d244025ae0a5cd2b8ce895bbd5050e5b49c10
Eduardo Ribeiro <eduardoribeiro17@gmail.com>
2020-03-27 14:08:53 -0300

    Stop for kernel upgrade

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('o nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" name="email" placeholder="Seu e-mail" />
-        <input
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input
           type="password"
           name="password"
           placeholder="Sua senha secreta"
         />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
